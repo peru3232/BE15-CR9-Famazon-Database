269e62e6b0236759c03ce1fbe5bf470ef1f5f0cd
peru3232 <prw@qmail.com.de>
2022-03-12 18:14:59 +0100

    only 5th :-(

--- BE15_CR9_famazon_peter_queries.sql
+++ BE15_CR9_famazon_peter_queries.sql
@@ -1,10 +1,10 @@
--- Querry 1: how many different products are actually offer from company Rosenstengel
+-- Querry 1: how many different products are actually offer from company Rosenstengel?
 SELECT COUNT(i.fk_supplier_id) FROM `items-stock` i
 JOIN supplier s ON i.fk_supplier_id = s.supplier_id
 WHERE s.name = "Rosenstengel"
 
 
--- Querry 2: what products with quantity are shipped between "2022-02-04" and "2022-03-01"
+-- Querry 2: what products with quantity are shipped between "2022-02-04" and "2022-03-01"?
 SELECT p.name ,od.quantity
 FROM orderdetails od JOIN `items-stock` i 
 ON od.fk_item_id = i.item_id
@@ -14,7 +14,7 @@ WHERE od.fk_invoice_id in (
     WHERE s.shippingDate BETWEEN "2022-02-04" and "2022-03-01")
 
 
--- Querry 3: how many products was delivered since 1st of March 22
+-- Querry 3: how many products was delivered since 1st of March 22?
 SELECT SUM(od.quantity)
 FROM orderdetails od JOIN `items-stock` i 
 ON od.fk_item_id = i.item_id
@@ -32,5 +32,17 @@ WHERE od.fk_order_id in (
 	SELECT o.order_id from `order` o
 	JOIN customer c ON c.userName = o.fk_userName
     WHERE c.firstName = "Jenny" AND c.lastName = "TestChar")
+
+
+-- Querry 5: which products was delivered to "Salzburg"?
+SELECT p.name FROM shipping s
+JOIN invoice iv on iv.invoice_id = s.fk_invoice_id
+JOIN orderdetails od on od.fk_invoice_id = iv.invoice_id
+JOIN `order` o on o.order_id = od.fk_invoice_id
+JOIN customer c on o.fk_userName = c.userName
+JOIN area a on c.fk_zip = a.zip
+JOIN `items-stock` i on i.item_id = od.fk_item_id
+JOIN products p on p.product_id = i.fk_product_id
+WHERE a.city = "Salzburg"
 
 
